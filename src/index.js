713d0b5b9186ffc1b7160129bd6cdc7e75326e4a
asteph <alissa.stephens1@gmail.com>
2020-10-29 12:15:26 -0500

    implement query and mutation resolvers with data in memory

--- src/index.js
+++ src/index.js
@@ -6,17 +6,50 @@ const { ApolloServer, gql } = require('apollo-server-express');
 
 const port = process.env.PORT || 4000;
 
+let notes = [
+  { id: '1', content: 'This is a note', author: 'Alissa Stephens' },
+  { id: '2', content: 'I just had a baby!', author: 'Julie Hudson' },
+  { id: '3', content: 'My power is out!', author: 'Vonda Stephens' }
+]
+
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
+  type Note {
+    id: ID!
+    content: String!
+    author: String!
+  }
+
   type Query {
-    hello: String
+    hello: String!
+    notes: [Note!]!
+    note(id: ID!): Note!
+  }
+
+  type Mutation {
+    newNote(content: String!): Note!
   }
 `;
 
 // Provide resolver functions for our schema fields
 const resolvers = {
   Query: {
-    hello: () => 'Hello world!'
+    hello: () => 'Hello world!',
+    notes: () => notes,
+    note: (parent, args) => {
+      return notes.find(note => note.id === args.id);
+    }
+  },
+  Mutation: {
+    newNote: (parent, args) => {
+      let noteValue = {
+        id: String(notes.length + 1),
+        content: args.content,
+        author: 'Alissa Stephens'
+      };
+      notes.push(noteValue);
+      return noteValue;
+    }
   }
 };
 
