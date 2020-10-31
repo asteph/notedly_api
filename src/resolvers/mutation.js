74d99a85464d1042c0c1fbf4c69dc1d4c6b44a85
asteph <alissa.stephens1@gmail.com>
2020-10-30 19:36:21 -0500

    adds CRUD functionality to the API adds custom scalar to the schema (DateTime in this instance)

---
+++ src/resolvers/mutation.js
@@ -0,0 +1,23 @@
+module.exports = {
+  newNote: async (parent, args, { models }) => {
+    return await models.Note.create({
+      content: args.content,
+      author: 'Alissa Stephens'
+    });
+  },
+  deleteNote: async (parent, { id }, { models }) => {
+    try {
+      await models.Note.findOneAndRemove({ _id: id });
+      return true;
+    } catch (error) {
+      return false;
+    }
+  },
+  updateNote: async (parent, { content, id }, { models }) => {
+    return await models.Note.findOneAndUpdate(
+      { _id: id },
+      { $set: { content } },
+      { new: true }
+    );
+  }
+};
